da821cd9471502f71214ad9ebc909979fbacbf8e
Nauzet <nauuuni@gmail.com>
2022-12-27 15:46:16 +0000

    Commit Kata5P1 v1

---
+++ src/kata5p1/Kata5P1.java
@@ -0,0 +1,43 @@
+
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kata5P1 {
+    
+    public static void main(String[] args) {
+        selectAll();
+    }
+    
+    private static Connection connect() {
+        String url = "jdbc:sqlite:KATA5.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+    public static void selectAll(){
+        String sql = "SELECT * FROM PEOPLE";
+        try (Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                rs.getString("Name") + "\t" +
+                rs.getString("Apellido") + "\t" +
+                rs.getString("Departamento") + "\t");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
